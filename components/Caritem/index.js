49c0269a8858ab957d3681eae034b54c79cf25bc
AdioReact <adio@dataluxtt.com>
2021-06-27 09:31:46 -0400

    update

--- components/Caritem/index.js
+++ components/Caritem/index.js
@@ -4,31 +4,32 @@ import styles from "./styles";
 import StyledButton from "../StyledButton";
 
 const CarItem = (props) => {
+  const { name, tagline, image } = props;
+
   return (
     <View style={styles.carContainer}>
-      <ImageBackground
-        source={require("../../assets/images/ModelX.jpeg")}
-        style={styles.image}
-      />
+      <ImageBackground source={image} style={styles.image} />
       <View style={styles.titles}>
-        <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at $69,420</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>{tagline}</Text>
       </View>
 
-      <StyledButton
-        type="primary"
-        content={"Custom Order"}
-        onPress={() => {
-          console.warn("Custom Order Was Pressed");
-        }}
-      />
-      <StyledButton
-        type="secondary"
-        content={"Existing Inventory"}
-        onPress={() => {
-          console.warn("Existing Inventory Was Pressed");
-        }}
-      />
+      <View style={styles.buttonsContainer}>
+        <StyledButton
+          type="primary"
+          content={"Custom Order"}
+          onPress={() => {
+            console.warn("Custom Order Was Pressed");
+          }}
+        />
+        <StyledButton
+          type="secondary"
+          content={"Existing Inventory"}
+          onPress={() => {
+            console.warn("Existing Inventory Was Pressed");
+          }}
+        />
+      </View>
     </View>
   );
 };
--- components/Caritem/styles.js
+++ components/Caritem/styles.js
@@ -11,6 +11,7 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     position: "absolute",
   },
+  buttonsContainer: { position: "absolute", bottom: 50, width: "100%" },
 });
 
 export default styles;
