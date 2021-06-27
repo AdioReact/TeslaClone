b4de09848eac87528c6d3a990afe5e1bd24e10b9
AdioReact <adio@dataluxtt.com>
2021-06-27 09:23:17 -0400

    update

--- components/Caritem/index.js
+++ components/Caritem/index.js
@@ -15,7 +15,20 @@ const CarItem = (props) => {
         <Text style={styles.subtitle}>Starting at $69,420</Text>
       </View>
 
-      <StyledButton />
+      <StyledButton
+        type="primary"
+        content={"Custom Order"}
+        onPress={() => {
+          console.warn("Custom Order Was Pressed");
+        }}
+      />
+      <StyledButton
+        type="secondary"
+        content={"Existing Inventory"}
+        onPress={() => {
+          console.warn("Existing Inventory Was Pressed");
+        }}
+      />
     </View>
   );
 };
